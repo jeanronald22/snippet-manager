6b87c3e7b29d572b6c4a7a48d7615162ac2a257d
jean ronald <jeroboumg@gmail.com>
2025-06-21 09:46:59 +0100

    mise en place de la gestion des snippets, tags, categories et langages

--- src/snippet_manager/settings.py
+++ src/snippet_manager/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'users',
-    'django_extensions'
+    'django_extensions',
+    'snippets'
 ]
 
 REST_FRAMEWORK = {
--- src/users/urls.py
+++ src/users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserViewSet
 
 router = DefaultRouter()
-router.register(r'user', UserViewSet, basename="auth")
+router.register(r'users', UserViewSet, basename="user")
 urlpatterns = [path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
                path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
                path('', include(router.urls))]

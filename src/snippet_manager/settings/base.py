9ceb39a685db3a4f056f3eb1ebf1d9ddc5a5b854
jean ronald <jeroboumg@gmail.com>
2025-07-04 11:09:14 +0100

    prod/dev

--- src/snippet_manager/settings/base.py
+++ src/snippet_manager/settings/base.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 from datetime import timedelta
 from pathlib import Path
-from decouple import config, Csv
+from decouple import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent

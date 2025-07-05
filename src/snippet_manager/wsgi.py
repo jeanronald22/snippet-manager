f7c697144e693da1d1d53d53ba7d77761105b275
jean ronald <jeroboumg@gmail.com>
2025-07-05 14:32:18 +0100

    update de asgi

--- src/snippet_manager/wsgi.py
+++ src/snippet_manager/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snippet_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'snippet_manager.settings.local'))
 
 from whitenoise import WhiteNoise
+
 application = WhiteNoise(get_wsgi_application())
